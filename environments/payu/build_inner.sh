ac82c8b7edfc3170ce2d95c2d278999447993968
Jo Basevi <johanna.basevi@anu.edu.au>
2024-10-02 17:33:56 +1000

    Update payu build script to patch python shebang on payu command to use launcher script

--- environments/payu/build_inner.sh
+++ environments/payu/build_inner.sh
@@ -1,8 +1,12 @@
 ### Custom install inner to build jupyter lab extensions
 
-set +u
-eval "$( ${MAMBA} shell hook --shell bash)"
-micromamba activate "${CONDA_INSTALLATION_PATH}/envs/${FULLENV}"
-set -u
+# set +u
+# eval "$( ${MAMBA} shell hook --shell bash)"
+# micromamba activate "${CONDA_INSTALLATION_PATH}/envs/${FULLENV}"
+# set -u
 
-jupyter lab build
+# jupyter lab build
+
+# Patch payu shebang header with outer python executable that launches a container when run.
+# This means when payu submits qsub commands (e.g. payu run), it uses this python executable and launches a container on PBS job
+sed -i "1s|^#!/.*$|#!${CONDA_SCRIPT_PATH}/${FULLENV}.d/bin/python|" "${CONDA_INSTALLATION_PATH}/envs/${FULLENV}/bin/payu"
