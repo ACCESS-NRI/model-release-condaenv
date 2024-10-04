b87e7560fb2323742e2c40d1a5347077d1b8366b
Jo Basevi <johanna.basevi@anu.edu.au>
2024-10-04 12:39:06 +1000

    Install official latest micromamba when there's no pre-existing conda base
    Removed the modified micromamba as at this stage we might not need compatibility with nb_conda_kernals

--- scripts/initialise.sh
+++ scripts/initialise.sh
@@ -17,9 +17,10 @@ function inner() {
 
     mkdir -p "${CONDA_INSTALLATION_PATH}"
     pushd "${CONDA_INSTALLATION_PATH}"
-    #curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+
+    curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
     ### Modified micromamba for compatibility with nb_conda_kernels
-    curl -Ls https://dsroberts.github.io/mamba/latest | tar -xvj bin/micromamba bin/activate
+    # curl -Ls https://dsroberts.github.io/mamba/latest | tar -xvj bin/micromamba bin/activate
     popd
 
     mkdir -p "${CONDA_SCRIPT_PATH}"/overrides
