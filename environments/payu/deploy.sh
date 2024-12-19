6ced8d047a8928af714b35e421e6e7151f3eb672
Jo Basevi <johanna.basevi@anu.edu.au>
2024-12-19 15:52:36 +1100

    Change APPS_OWNER_GROUP to APPS_OWNER (user rather than a group)

--- environments/payu/deploy.sh
+++ environments/payu/deploy.sh
@@ -1,10 +1,12 @@
-### Update stable if necessary
-CURRENT_STABLE=$( get_aliased_module "${MODULE_NAME}" "${MODULE_PATH}" )
-NEXT_STABLE="${ENVIRONMENT}-${STABLE_VERSION}"
+# UNCOMMENT once payu is moved out of pre-release as default payu module in prerelease should remain payu/dev
 
-if ! [[ "${CURRENT_STABLE}" == "${MODULE_NAME}/${STABLE_VERSION}" ]]; then
-    echo "Updating stable environment to ${NEXT_STABLE}"
-    write_modulerc_stable "${STABLE_VERSION}" "${ENVIRONMENT}" "${CONDA_MODULE_PATH}" "${MODULE_NAME}"
-    symlink_atomic_update "${CONDA_INSTALLATION_PATH}"/envs/"${ENVIRONMENT}" "${NEXT_STABLE}"
-    symlink_atomic_update "${CONDA_SCRIPT_PATH}"/"${ENVIRONMENT}".d "${NEXT_STABLE}".d
-fi
+### Update stable if necessary
+# CURRENT_STABLE=$( get_aliased_module "${MODULE_NAME}" "${MODULE_PATH}" )
+# NEXT_STABLE="${ENVIRONMENT}-${STABLE_VERSION}"
+
+# if ! [[ "${CURRENT_STABLE}" == "${MODULE_NAME}/${STABLE_VERSION}" ]]; then
+#     echo "Updating stable environment to ${NEXT_STABLE}"
+#     write_modulerc_stable "${STABLE_VERSION}" "${ENVIRONMENT}" "${CONDA_MODULE_PATH}" "${MODULE_NAME}"
+#     symlink_atomic_update "${CONDA_INSTALLATION_PATH}"/envs/"${ENVIRONMENT}" "${NEXT_STABLE}"
+#     symlink_atomic_update "${CONDA_SCRIPT_PATH}"/"${ENVIRONMENT}".d "${NEXT_STABLE}".d
+# fi
