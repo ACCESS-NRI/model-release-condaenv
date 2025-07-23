a83438730b341a546d4af3a6d3252ad1f4cc7900
Jo Basevi <johanna.basevi@anu.edu.au>
2025-07-23 16:45:18 +1000

    Update comment in payu-dev/build_inner script

--- environments/payu-dev/build_inner.sh
+++ environments/payu-dev/build_inner.sh
@@ -2,7 +2,7 @@
 
 # Fix shebang headers in payu entry points (issue with pip installed packages: https://github.com/ACCESS-NRI/MED-condaenv/issues/78)
 for file in ${CONDA_INSTALLATION_PATH}/envs/${FULLENV}/bin/payu*; do
-    # Using payu-* to modify payu-run, payu-collate, payu-sync files
+    # Using payu* to modify payu, payu-run, payu-collate, payu-sync files
     echo "Adding python header to $file"
     # Substitute the first line of file (e.g. 1s), if it starts with #! (regex ^#!/.*$),
     # with the python executable in the conda environment
